282c5285d4916e4d7e232d68f893896c9eb9ad66
Bowlofneighs <bowlofneighs@gmail.com>
2026-05-01 18:52:18 -0400

    feat: added glitching to greeting when scrolling into brutalist zone

---
+++ script.js
@@ -0,0 +1,39 @@
+const greeting = document.querySelector('#greeting');
+
+const fonts = ['Inter', 'monospace', 'serif'];
+const colors = ['#ffffff', '#ffff00', '#ff00ff', '#00ffff', '#0000ff', '#ff0000', '#00ff00', '#000000']
+
+let isGlitching = false;
+
+
+window.addEventListener('scroll', () => {
+  if(window.scrollY > 500){
+    greeting.classList.add('glitching');
+    isGlitching = true;
+  }  else if(window.scrollY < 500){
+    greeting.classList.remove('glitching');
+
+    isGlitching = false;
+
+    greeting.style.transform = '';
+    greeting.style.color = '';
+    greeting.style.fontFamily = '';
+  }
+});
+
+setInterval(() => {
+    if (isGlitching) {
+        greeting.classList.add('glitching');
+        greeting.style.setProperty('--glitch-trans-x', (Math.random() * 20 - 10) + 'px');
+        greeting.style.setProperty('--glitch-trans-y', (Math.random() * 10 - 5) + 'px');
+        greeting.style.setProperty('--glitch-skew-x', (Math.random() * 10 - 5) + 'deg');
+        greeting.style.setProperty('--glitch-skew-y', (Math.random() * 10 - 5) + 'deg');
+        greeting.style.setProperty('--glitch-color', colors[Math.floor(Math.random() * colors.length)]);
+    }
+}, 100);
+
+setInterval(()=> {
+    if (isGlitching) {
+        greeting.style.setProperty('--glitch-font-family', (Math.random() < 0.2 ? 'monospace' : ''));
+    }
+}, 500);
