a08a576a67b94dbb435d1b8ad9e0060227003afc
Bowlofneighs <bowlofneighs@gmail.com>
2026-05-05 21:31:46 -0400

    feat: snapped screen to black :yay:

--- glitch.js
+++ glitch.js
@@ -11,12 +11,13 @@ let L2Glitching = false;
 let currentFrame = 0;
 
 let previousScrollY = 0;
-let scrollLocked = false;
+window.scrollLocked = false;
 
+const Sawtooth60 = new Audio('60hz-sawtooth.wav');
 
 window.addEventListener('scroll', () => {
     if (L2GlitchStarted) {
-    if (scrollLocked) {
+    if (window.scrollLocked) {
       window.scrollTo(0, previousScrollY);
     }
     return;
@@ -36,14 +37,21 @@ window.addEventListener('scroll', () => {
   if(window.scrollY > 600 && !L2GlitchStarted){ //glitch takes over
     L2GlitchStarted = true;
     previousScrollY = window.scrollY + 10;
-    scrollLocked = true;
+    window.scrollLocked = true;
     setTimeout(() =>{
+      Sawtooth60.play();
       isGlitching = false;
       L2Glitching = true;
       level2Glitch()
+      setTimeout(() => {
+        console.log('clearing elements with data-section starter');
+        document.querySelectorAll('[data-section="starter"]').forEach(el => el.remove());
+      document.body.style.backgroundColor = '#000000';
+      window.dispatchEvent(new CustomEvent('boot-up'));
+      }, 7000);
     }, 1000);
   }
-  if(scrollLocked){
+  if(window.scrollLocked){
     window.scrollTo(0, previousScrollY);
   } else {
     previousScrollY = window.scrollY
